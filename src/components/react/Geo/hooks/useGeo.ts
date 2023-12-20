665822d10b0011b175b45e17af708482f1acb354
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-12-20 00:50:22 -0300

    Add Geo component and useGeo hook

---
+++ src/components/react/Geo/hooks/useGeo.ts
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+
+type GeoType = {
+  full: string | null
+  city: string | null
+  country: string | null
+  since: Date | null
+}
+
+function useGeo () {
+  const [geo, setGeo] = useState<GeoType>({
+    full: null,
+    city: null,
+    country: null,
+    since: null,
+  })
+
+  useEffect(() => {
+    fetch('https://geo.cgcapps.cl', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const from = `${data.city}, ${data.country}`
+        const diff = Math.round(Math.abs((new Date()).getTime() - (new Date(data.date)).getTime()) / 1000)
+        let minutes = Math.floor(diff / 60)
+        let hours = 0
+        let days = 0
+        if (minutes > 60) {
+          hours = Math.floor(minutes / 60)
+          minutes = minutes - (hours * 60)
+        }
+        if (hours > 24) {
+          days = Math.floor(hours / 24)
+          hours = hours - (days * 24)
+        }
+        const since = []
+        if (days > 0) {
+          since.push(`${days} d`)
+        }
+        if (hours > 0) {
+          since.push(`${hours} h`)
+        }
+        if (minutes > 0) {
+          since.push(`${minutes} m`)
+        }
+        setGeo({
+          full: `${from}, ${since.join(' ')}`,
+          city: data.city,
+          country: data.country,
+          since: new Date(data.date)
+        })
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+  return geo
+}
+
+export default useGeo
---
+++ src/components/react/Geo/index.tsx
@@ -0,0 +1,29 @@
+import useGeo from "./hooks/useGeo"
+
+const Geo = () => {
+  const geo = useGeo()
+  const showDetail = false
+
+  const handleClick = () => {
+  }
+
+  return (<>
+    <article className="relative text-[12px] md:text-md md:absolute text-center px-3 py-1 text-indigo-200/25 top-[64px] md:top-[25%] md:left-[-5rem] md:rotate-[270deg]"
+      onClick={handleClick}>
+      <p>{geo.full}</p>
+    </article>
+    {showDetail && (
+      <article className="w-[80%] md:w-[50%] h-40 bg-indigo-800/80 p-4 rounded-lg absolute left-[10%] md:left-[25%] top-[10%] md:top-[25%] z-20">
+        <h1 className="text-center text-3xl font-bold mb-3 uppercase">Last visit from</h1>
+        <p className="text-center">{geo.city}</p>
+        <p className="text-center">{geo.country}</p>
+        <p className="text-center">
+          <time dateTime={geo.since?.toISOString() || ''} >{geo.since?.toLocaleDateString()}</time>
+        </p>
+      </article>
+    )}
+  </>
+  )
+}
+
+export default Geo

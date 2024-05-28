9376c0e68c6d14d94caf94ed08dea53b76b470b0
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-05-28 11:59:19 -0400

    feat: Add Gester project to portfolio

--- src/types.ts
+++ src/types.ts
@@ -1,41 +1,42 @@
 export type Project = {
-  name: string
-  description: string
-  longDescription: string
-  image: string
-  video?: string
-  videoDescription?: string
-  slug: string
-  github?: string
-  website?: string
-  tags: string[]
-  stack: string[]
-}
+	name: string;
+	description: string;
+	longDescription: string;
+	image: string;
+	video?: string;
+	videoDescription?: string;
+	slug: string;
+	github?: string;
+	website?: string;
+	tags: string[];
+	stack: string[];
+	gallery?: string[];
+};
 
 export type Tool = {
-  name: string
-  description: string
-  slug: string
-}
+	name: string;
+	description: string;
+	slug: string;
+};
 
 export type SkillType = {
-  name: string
-  since: number
-}
+	name: string;
+	since: number;
+};
 
 export type getHeightProps = {
-  since: number
-  maxExperience: number
-}
+	since: number;
+	maxExperience: number;
+};
 
 export type StateType = {
-  show: null | string,
-  errors: { name: null | string, email: null | string, message: null | string }
-  canSendMail?: boolean
-  token?: string | null
-  formKey: number
-}
+	show: null | string;
+	errors: { name: null | string; email: null | string; message: null | string };
+	canSendMail?: boolean;
+	token?: string | null;
+	formKey: number;
+};
 export type ActionType = {
-  type: string,
-  payload?: string | null
-}
+	type: string;
+	payload?: string | null;
+};
